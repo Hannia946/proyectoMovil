31f216a1453bc524e9886fe23ffbc718113438bc
MarisolVilla <villamarisol392@gmail.com>
2025-06-10 21:04:53 -0600

    Modificaciones de fragments

--- app/src/main/java/com/example/proyecto/Repartidor/MainActivityRepartidor.kt
+++ app/src/main/java/com/example/proyecto/Repartidor/MainActivityRepartidor.kt
@@ -1,21 +1,62 @@
-package com.example.proyecto.Repartidor
-
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
 import com.example.proyecto.R
+import com.example.proyecto.Repartidor.nav_fragments_repartidor.FragmentInicioRepartidor
+import com.example.proyecto.cliente.nav_fragments_cliente.FragmentInicioCliente
+import com.example.proyecto.databinding.ActivityMainRepartidorBinding
+import com.google.android.material.navigation.NavigationView
 
-class MainActivityRepartidor : AppCompatActivity() {
+class MainActivityRepartidor : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var binding: ActivityMainRepartidorBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main_repartidor)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainRepartidorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        binding.navigationView.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            toolbar,
+            R.string.open_drawer,
+            R.string.close_drawer
+        )
+
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.op_inicio_repartidor -> {
+
+                replaceFragment(FragmentInicioRepartidor())
+            }
+
+            R.id.op_cerrarSesion_repartidor -> {
+                Toast.makeText(this, "Has cerrado sesión", Toast.LENGTH_SHORT).show()
+            }
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    private fun replaceFragment(fragmentInicioRepartidor: FragmentInicioRepartidor) {
+
+    }
+
 }
---
+++ app/src/main/java/com/example/proyecto/Repartidor/nav_fragments_repartidor/FragmentInicioRepartidor.kt
@@ -0,0 +1,22 @@
+package com.example.proyecto.Repartidor.nav_fragments_repartidor
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.proyecto.R
+
+class FragmentInicioRepartidor : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_inicio_repartidor, container, false)
+    }
+
+
+}
